39fa1e5bbaafd4b1c291fc7100e26d7d8cd37bd6
Mike Stone <michaeljacobstone@gmail.com>
2016-02-02 18:53:05 -0800

    Add cats color and age migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203023411) do
+ActiveRecord::Schema.define(version: 20160203024529) do
 
   create_table "cats", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "color"
+    t.decimal  "age"
   end
 
 end
